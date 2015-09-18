638aa990bd742c1d5bf0140c3181662b588d3dbc
acionescu <adrian.ionescu.consulting@gmail.com>
2015-09-18 08:51:28 +0300

    license, cleanup

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -Dlog4j.configuration=file:log4j.properties -Djgroups.udp.mcast_addr=228.11.11.10 -Djgroups.udp.mcast_port=51110 -cp .:target/dependency/* ro.zg.util.bootstrap.GenericBootstrap 
+java -Dlog4j.configuration=file:log4j.properties -Djgroups.udp.mcast_addr=228.11.11.10 -Djgroups.udp.mcast_port=51110 -cp .:target/dependency/* net.segoia.util.bootstrap.GenericBootstrap 
 bootstrap.properties
